94b9569368117fb612d93f7d1ec27c04c1329899
unknown <j.donovan@Muninn.Virtalis.local>
2012-05-17 17:37:00 +0100

    Added basic label handling. Tidied up some code. Fixed some bugs.

--- gridpluginlayer.py
+++ gridpluginlayer.py
@@ -27,22 +27,66 @@ from qgis.core import QGis
 
 from gridpropertiesdialog import GridPropertiesDialog
 
+class QgsVector(object):
+  """2D vector class with (almost) the same signature as the QGIS one."""
+  def __init__(self, x=0.0, y=0.0):
+    self.x = x
+    self.y = y
+
+  def __neg__(self):
+    return QgsVector(-self.x, -self.y)
+
+  def __mul__(self, scalar):
+    return QgsVector(self.x * scalar, self.y * scalar)
+
+  def __div__(self, scalar):
+    return QgsVector(self.x, self.y) * (1.0 / scalar)
+
+  def __add__(self, v):
+    return QgsVector(self.x + v.x, self.y + v.y)
+
+  # Vector dot product.
+  def __and__(self, v):
+    return self.x * v.x + self.y * v.y
+
+  def length(self):
+    return math.sqrt(self.x * self.x + self.y * self.y)
+
+  def perpVector(self):
+    return QgsVector(-self.y, self.x)
+
+  def angle(self, v=None):
+    if v is None:
+      ang = math.atan2(self.y, self.x)
+      return ang + 2.0 * math.pi if ang < 0.0 else ang
+    else:
+      return v.angle() - self.angle()
+
+  def rotateBy(self, rot):
+    ang = math.atan2(self.y, self.x) + rot
+    length = self.length()
+    return QgsVector(length * math.cos(ang), length * math.sin(ang))
+
+  def normal(self):
+    if self.length() == 0.0:
+      raise
+
+    return QgsVector(self.x, self.y) / self.length()
+
 class GridPluginLayer(core.QgsPluginLayer):
     LAYER_TYPE = 'grid'
+
+    _fmap = {0: core.QgsField('cell_num', QtCore.QVariant.Int, 'integer', 8),
+            1: core.QgsField('angle', QtCore.QVariant.Double, 'double', 8, 4),
+            2: core.QgsField('ordinate', QtCore.QVariant.Double, 'double', 8, 4)
+           }
+
 
     def __init__(self):
         core.QgsPluginLayer.__init__(self, GridPluginLayer.LAYER_TYPE,
                                      'Grid overlay')
-        self.setValid(False)
+        self.setValid(True)
 
-        proj = core.QgsProject.instance()
-        # Default CRS: 3452 == EPSG:4326
-        srid = proj.readNumEntry('SpatialRefSys', '/ProjectCRSID', 3452)[0]
-        # It's OK to use the internal ID because it's a one-time
-        # initialisation straight from the current project.
-        crs = core.QgsCoordinateReferenceSystem(srid, core.QgsCoordinateReferenceSystem.InternalCrsId)
-        self.setCrs(crs)
-        
         props = {'width':'0', 'color':'0,255,0'}
         self.symbol = core.QgsLineSymbolV2.createSimple(props)
 
@@ -55,10 +99,12 @@ class GridPluginLayer(core.QgsPluginLayer):
         self.cellSizeY = 10.0
         self.baselineAngle = 0.0
         self.grid = []
-        self.generateGrid()
-
-    def draw(self, renderContext):
-        mapToPixel = renderContext.mapToPixel()
+        self.label_features = []
+        self.label_attributes = core.QgsLabelAttributes()
+        self.draw_labels = False
+        self.label_type = 0
+        self.label_precision = 0
+        self.label_orientation = 0
 
         proj = core.QgsProject.instance()
         # Default CRS: 3452 == EPSG:4326
@@ -66,7 +112,16 @@ class GridPluginLayer(core.QgsPluginLayer):
         # It's OK to use the internal ID because it's a one-time
         # initialisation straight from the current project.
         crs = core.QgsCoordinateReferenceSystem(srid, core.QgsCoordinateReferenceSystem.InternalCrsId)
-        
+        self.setCrs(crs)
+
+    def draw(self, renderContext):
+        mapToPixel = renderContext.mapToPixel()
+
+        proj = core.QgsProject.instance()
+        # Default CRS: 3452 == EPSG:4326
+        srid = proj.readNumEntry('SpatialRefSys', '/ProjectCRSID', 3452)[0]
+        crs = core.QgsCoordinateReferenceSystem(srid, core.QgsCoordinateReferenceSystem.InternalCrsId)
+
         xform = core.QgsCoordinateTransform(self.crs(), crs)
 
         self.symbol.startRender(renderContext)
@@ -84,60 +139,83 @@ class GridPluginLayer(core.QgsPluginLayer):
                     self.symbol.renderPolyline(polyline, None, renderContext)
 
         self.symbol.stopRender(renderContext)
-
         return True
+
+    def drawLabels(self, renderContext):
+        if self.draw_labels:
+            for feat in self.label_features:
+              label = core.QgsLabel(GridPluginLayer._fmap)
+              label.labelAttributes().setFamily(self.label_attributes.family())
+              label.labelAttributes().setBold(self.label_attributes.bold())
+              label.labelAttributes().setItalic(self.label_attributes.italic())
+              label.labelAttributes().setUnderline(self.label_attributes.underline())
+              label.labelAttributes().setStrikeOut(self.label_attributes.strikeOut())
+              label.labelAttributes().setSize(self.label_attributes.size(), core.QgsLabelAttributes.PointUnits)
+              label.labelAttributes().setColor(self.label_attributes.color())
+              label.setLabelField(core.QgsLabel.Text, 2)
+              label.setLabelField(core.QgsLabel.Angle, 1)
+              label.renderLabel(renderContext, feat, False)
 
     def generateGrid(self):
         self.grid = []
+        self.label_features = []
 
-        # Angle of the baseline, clockwise.
-        angleSin = math.sin(math.radians(360.0 - self.baselineAngle))
-        angleCos = math.cos(math.radians(360.0 - self.baselineAngle))
+        baseVec = QgsVector(1.0, 0.0).rotateBy(math.radians(360.0 - self.baselineAngle)) * self.cellSizeX
+        perpVec = baseVec.perpVector().normal() * self.cellSizeY
 
         # Generate horizontal lines as a piecewise curve.
         for h in xrange(self.gridOffsetY, self.numCellsY + self.gridOffsetY + 1):
             line = []
-            veca = core.QgsPoint(self.cellSizeY * -angleSin * h, self.cellSizeY * angleCos * h)
 
             for l in xrange(self.gridOffsetX, self.numCellsX + self.gridOffsetX + 1):
-                vecb = core.QgsPoint(self.cellSizeX * angleCos * l, self.cellSizeX * angleSin * l)
-                line.append(core.QgsPoint(self.origin.x() + veca.x() + vecb.x(), self.origin.y() + veca.y() + vecb.y()))
+                bar = (perpVec * h) + (baseVec * l)
+                line.append(core.QgsPoint(self.origin.x() + bar.x, self.origin.y() + bar.y))
 
             self.grid.append(line)
 
         # Generate vertical lines as a piecewise curve.
         for v in xrange(self.gridOffsetX, self.numCellsX + self.gridOffsetX + 1):
             line = []
-            veca = core.QgsPoint(self.cellSizeX * angleCos * v, self.cellSizeX * angleSin * v)
 
             for l in xrange(self.gridOffsetY, self.numCellsY + self.gridOffsetY + 1):
-                vecb = core.QgsPoint(self.cellSizeY * -angleSin * l, self.cellSizeY * angleCos * l)
-                line.append(core.QgsPoint(self.origin.x() + veca.x() + vecb.x(), self.origin.y() + veca.y() + vecb.y()))
+                bar = (baseVec * v) + (perpVec * l)
+                line.append(core.QgsPoint(self.origin.x() + bar.x, self.origin.y() + bar.y))
 
             self.grid.append(line)
-        
-        x = []
-        y = []
-        x.append((self.gridOffsetX * self.cellSizeX * angleCos) +
-                 (self.gridOffsetY * self.cellSizeY * -angleSin))
-        x.append(((self.numCellsX + self.gridOffsetX) * self.cellSizeX * angleCos) +
-                 (self.gridOffsetY * self.cellSizeY * -angleSin))
-        x.append((self.gridOffsetX * self.cellSizeX * angleCos) +
-                 (self.gridOffsetY * self.cellSizeY * -angleSin))
-        x.append(((self.numCellsX + self.gridOffsetX) * self.cellSizeX * angleCos) +
-                 ((self.numCellsY + self.gridOffsetY) * self.cellSizeY * -angleSin))
 
-        y.append((self.gridOffsetX * self.cellSizeX * angleSin) +
-                 (self.gridOffsetY * self.cellSizeY * angleCos))
-        y.append((self.gridOffsetX * self.cellSizeX * angleSin) +
-                 ((self.numCellsY + self.gridOffsetY) * self.cellSizeY * angleCos))
-        y.append(((self.numCellsX + self.gridOffsetX) * self.cellSizeX * angleSin) +
-                 (self.gridOffsetY * self.cellSizeY * angleCos))
-        y.append(((self.numCellsX + self.gridOffsetX) * self.cellSizeX * angleSin) +
-                 ((self.numCellsY + self.gridOffsetY) * self.cellSizeY * angleCos))
-                 
-        self.setExtent(core.QgsRectangle(min(x) + self.origin.x(), min(y) + self.origin.y(), max(x) + self.origin.x(), max(y) + self.origin.y()))
-        
+        for cell in xrange(0, len(self.grid[0])):
+          feat = core.QgsFeature()
+          feat.addAttribute(0, cell)
+          feat.addAttribute(1, math.degrees(baseVec.angle()))
+          feat.addAttribute(2, self.grid[0][cell].x())
+          feat.setGeometry(core.QgsGeometry().fromPoint(self.grid[0][cell]))
+          self.label_features.append(feat)
+
+        for cell in xrange(0, len(self.grid[self.numCellsY + 1])):
+          feat = core.QgsFeature()
+          feat.addAttribute(0, cell)
+          feat.addAttribute(1, math.degrees(baseVec.angle()))
+          feat.addAttribute(2, self.grid[self.numCellsY + 1][cell].y())
+          feat.setGeometry(core.QgsGeometry().fromPoint(self.grid[self.numCellsY + 1][cell]))
+          self.label_features.append(feat)
+
+        lower_left = baseVec * self.gridOffsetX
+        lower_right = baseVec * (self.numCellsX + self.gridOffsetX)
+        upper_left = perpVec * (self.numCellsY + self.gridOffsetY)
+        upper_right = lower_right + upper_left
+
+        minx = min(lower_left.x, min(lower_right.x, min(upper_left.x, upper_right.x)))
+        miny = min(lower_left.y, min(lower_right.y, min(upper_left.y, upper_right.y)))
+        maxx = max(lower_left.x, max(lower_right.x, max(upper_left.x, upper_right.x)))
+        maxy = max(lower_left.y, max(lower_right.y, max(upper_left.y, upper_right.y)))
+
+        self.setExtent(core.QgsRectangle(minx + self.origin.x(), miny + self.origin.y(), maxx + self.origin.x(), maxy + self.origin.y()))
+
+    def setCrs(self, crs):
+        core.QgsPluginLayer.setCrs(self, crs)
+        self.generateGrid()
+        self.setCacheImage(None)
+        self.emit(QtCore.SIGNAL('repaintRequested()'))
 
     def readXml(self, node):
         org_x = float(node.toElement().attribute('origin_x', '0.0'))
@@ -150,12 +228,9 @@ class GridPluginLayer(core.QgsPluginLayer):
         self.cellSizeX = float(node.toElement().attribute('cell_size_x', '10.0'))
         self.cellSizeY = float(node.toElement().attribute('cell_size_y', '10.0'))
         self.baselineAngle = float(node.toElement().attribute('baseline_angle', '0.0'))
-
-        symbolElement = node.firstChildElement('symbol')
-        if symbolElement is not None and symbolElement.attribute('name') == 'grid_lines':
-          self.symbol = core.QgsSymbolLayerV2Utils.loadSymbol(symbolElement)
-
         self.generateGrid()
+        self.readSymbology(node, None)
+
         return True
 
     def writeXml(self, node, doc):
@@ -172,36 +247,36 @@ class GridPluginLayer(core.QgsPluginLayer):
         element.setAttribute("cell_size_x", str(self.cellSizeX))
         element.setAttribute("cell_size_y", str(self.cellSizeY))
         element.setAttribute("baseline_angle", str(self.baselineAngle))
-        symbolElement = core.QgsSymbolLayerV2Utils.saveSymbol('grid_lines', self.symbol, doc, None)
-        node.appendChild(symbolElement)
+        self.writeSymbology(node, doc, None)
 
         return True
 
-    def showDialog(self):
-        dlg = GridPropertiesDialog(self.symbol)
-        dlg.ui.editOriginX.setText(str(self.origin.x()))
-        dlg.ui.editOriginY.setText(str(self.origin.y()))
-        dlg.ui.spinCountX.setValue(self.numCellsX)
-        dlg.ui.spinCountY.setValue(self.numCellsY)
-        dlg.ui.spinOffsetX.setValue(self.gridOffsetX)
-        dlg.ui.spinOffsetY.setValue(self.gridOffsetY)
-        dlg.ui.spinCellSizeX.setValue(self.cellSizeX)
-        dlg.ui.spinCellSizeY.setValue(self.cellSizeY)
-        dlg.ui.spinAngle.setValue(self.baselineAngle)
-        dlg.show()
-        result = dlg.exec_()
-        
-        if result == 1:
-            self.setValid(True)
-            self.origin = core.QgsPoint(float(dlg.ui.editOriginX.text()), float(dlg.ui.editOriginY.text()))
-            self.numCellsX = dlg.ui.spinCountX.value()
-            self.numCellsY = dlg.ui.spinCountY.value()
-            self.gridOffsetX = dlg.ui.spinOffsetX.value()
-            self.gridOffsetY = dlg.ui.spinOffsetY.value()
-            self.cellSizeX = dlg.ui.spinCellSizeX.value()
-            self.cellSizeY = dlg.ui.spinCellSizeY.value()
-            self.baselineAngle = dlg.ui.spinAngle.value()
+    def readSymbology(self, node, errorMessage):
+        symbolElement = node.firstChildElement('symbol')
 
-            self.generateGrid()
+        if symbolElement is not None and symbolElement.attribute('name') == 'grid_lines':
+            self.symbol = core.QgsSymbolLayerV2Utils.loadSymbol(symbolElement)
             self.setCacheImage(None)
             self.emit(QtCore.SIGNAL('repaintRequested()'))
+            return True
+        else:
+            return False
+
+    def writeSymbology(self, node, doc, errorMessage):
+        symbolElement = core.QgsSymbolLayerV2Utils.saveSymbol('grid_lines', self.symbol, doc, None)
+        node.appendChild(symbolElement)
+        return True
+
+    def showDialog(self):
+        dlg = GridPropertiesDialog(self)
+
+        dlg.show()
+        result = dlg.exec_()
+
+        if result == 1:
+            self.generateGrid()
+            self.setValid(True)
+            self.setCacheImage(None)
+            self.emit(QtCore.SIGNAL('repaintRequested()'))
+        else:
+            self.setValid(False)
--- gridpropertiesdialog.py
+++ gridpropertiesdialog.py
@@ -27,17 +27,68 @@ from ui_grid_properties import Ui_GridProperties
 
 class GridPropertiesDialog(QtGui.QDialog):
 
-    def __init__(self, symbol):
+    def __init__(self, gridlayer):
         QtGui.QDialog.__init__(self)
         # Set up the user interface from Designer.
         self.ui = Ui_GridProperties()
         self.ui.setupUi(self)
+
+        self.gridlayer = gridlayer
+        
         validator = QtGui.QDoubleValidator(-10000000.0, 10000000.0, 6, None)
         self.ui.editOriginX.setValidator(validator)
         self.ui.editOriginY.setValidator(validator)
-        self.symbol = symbol
         self.ui.btnStyle.clicked.connect(self.chooseStyle)
-    
+        self.ui.btnFont.clicked.connect(self.chooseFont)
+        self.ui.btnColour.clicked.connect(self.chooseColour)
+
+        self.symbol = gridlayer.symbol.clone()
+        self.label_attributes = gridlayer.label_attributes
+        
+        # Store attributes in case the user changes font attributes but cancels the dialog box.
+        self.family = gridlayer.label_attributes.family()
+        self.bold = gridlayer.label_attributes.bold()
+        self.italic = gridlayer.label_attributes.italic()
+        self.underline = gridlayer.label_attributes.underline()
+        self.strikeOut = gridlayer.label_attributes.strikeOut()
+        self.size = gridlayer.label_attributes.size()
+        self.color = gridlayer.label_attributes.color()
+
+        self.ui.boxLabels.setChecked(gridlayer.draw_labels)
+        self.ui.editOriginX.setText(str(gridlayer.origin.x()))
+        self.ui.editOriginY.setText(str(gridlayer.origin.y()))
+        self.ui.spinCountX.setValue(gridlayer.numCellsX)
+        self.ui.spinCountY.setValue(gridlayer.numCellsY)
+        self.ui.spinOffsetX.setValue(gridlayer.gridOffsetX)
+        self.ui.spinOffsetY.setValue(gridlayer.gridOffsetY)
+        self.ui.spinCellSizeX.setValue(gridlayer.cellSizeX)
+        self.ui.spinCellSizeY.setValue(gridlayer.cellSizeY)
+        self.ui.spinAngle.setValue(gridlayer.baselineAngle)
+        
+    def accept(self):
+        self.gridlayer.origin = core.QgsPoint(float(self.ui.editOriginX.text()), float(self.ui.editOriginY.text()))
+        self.gridlayer.numCellsX = self.ui.spinCountX.value()
+        self.gridlayer.numCellsY = self.ui.spinCountY.value()
+        self.gridlayer.gridOffsetX = self.ui.spinOffsetX.value()
+        self.gridlayer.gridOffsetY = self.ui.spinOffsetY.value()
+        self.gridlayer.cellSizeX = self.ui.spinCellSizeX.value()
+        self.gridlayer.cellSizeY = self.ui.spinCellSizeY.value()
+        self.gridlayer.baselineAngle = self.ui.spinAngle.value()
+        self.gridlayer.draw_labels = self.ui.boxLabels.isChecked()
+        self.gridlayer.symbol = self.symbol.clone()
+        QtGui.QDialog.accept(self)
+
+    def reject(self):
+        # Restore original label attributes.
+        self.gridlayer.label_attributes.setFamily(self.family)
+        self.gridlayer.label_attributes.setBold(self.bold)
+        self.gridlayer.label_attributes.setItalic(self.italic)
+        self.gridlayer.label_attributes.setUnderline(self.underline)
+        self.gridlayer.label_attributes.setStrikeOut(self.strikeOut)
+        self.gridlayer.label_attributes.setSize(self.size, core.QgsLabelAttributes.PointUnits)
+        self.gridlayer.label_attributes.setColor(self.color)
+        QtGui.QDialog.reject(self)
+
     def chooseStyle(self):
         if QGis.QGIS_VERSION_INT < 10800:
             dlg = gui.QgsSymbolV2SelectorDialog(self.symbol,
@@ -45,6 +96,29 @@ class GridPropertiesDialog(QtGui.QDialog):
         else:
             dlg = gui.QgsSymbolV2SelectorDialog(self.symbol,
                                                 core.QgsStyleV2.defaultStyle(),
-                                                None )
+                                                None)
         dlg.show()
         dlg.exec_()
+
+    def chooseFont(self):
+        dlg = QtGui.QFontDialog()
+        dlg.show()
+        result = dlg.exec_()
+        
+        if result == 1:
+            font = dlg.selectedFont()
+            self.label_attributes.setFamily(font.family())
+            self.label_attributes.setBold(font.bold())
+            self.label_attributes.setItalic(font.italic())
+            self.label_attributes.setUnderline(font.underline())
+            self.label_attributes.setStrikeOut(font.strikeOut())
+            self.label_attributes.setSize(font.pointSizeF(), core.QgsLabelAttributes.PointUnits)
+
+    def chooseColour(self):
+        dlg = QtGui.QColorDialog(self.label_attributes.color())
+        dlg.setOptions(QtGui.QColorDialog.ShowAlphaChannel)
+        dlg.show()
+        result = dlg.exec_()
+        
+        if result == 1:
+            self.label_attributes.setColor(dlg.selectedColor())
